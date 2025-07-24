233341d886612f55948ad2f542b9dfa32d7e5e53
Milo <milo.grv@gmail.com>
2025-07-24 10:58:47 +0200

    image in /floposite embed

--- index.js
+++ index.js
@@ -1405,6 +1405,9 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
               title: 'FlopoSite',
               description: 'L\'officiel et très goatesque site de FlopoBot.',
               color: 0x6571F3,
+              image: {
+                url: process.env.API_URL + '/public/images/curry.png'
+              }
             }
           ],
           components: [
